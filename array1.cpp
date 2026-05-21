70df2697df44ec0743492f1be9c367f37424ed31
“abeerscode” <md.abeerhasan73@gmail.com>
2026-05-22 00:03:59 +0600

    Matrix

---
+++ array1.cpp
@@ -0,0 +1,20 @@
+// Matrix 3x3
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row = 3;
+    int col = 3;
+
+    int value = 0;
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            value++;
+            cout << value << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
