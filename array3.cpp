e951e53753fa87da0c2ec05f76e218262ea29ce7
“abeerscode” <md.abeerhasan73@gmail.com>
2026-05-22 01:13:02 +0600

    Max element of 2D  matrix

---
+++ array3.cpp
@@ -0,0 +1,36 @@
+// Max element of a matrix
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row, col;
+
+    cout << "Enter row number: ";
+    cin >> row;
+    cout << "Enter column number: ";
+    cin >> col;
+
+    int arr[100][100];
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout << "Element - (" << i+1 << "," << j+1 << ") : ";
+            cin >> arr[i][j];
+        }
+    }
+
+    int max = arr[0][0];
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(arr[i][j] > max){
+                max = arr[i][j];
+            }
+        }
+    }
+
+    cout << "Max element of the matrix: " << max;
+
+    return 0;
+}
