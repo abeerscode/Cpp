69de37b2ae306b11ecc365279fa0f379b3c566ce
“abeerscode” <md.abeerhasan73@gmail.com>
2026-05-23 20:18:15 +0600

    Sum of the Diagonal of 2D Arraay

---
+++ array5.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row, col;
+
+    cout << "Enter row number: ";
+    cin >> row;
+
+    cout << "Enter column number: ";
+    cin >> col;
+
+    int arr[100][100];
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout << "Element - (" << i+1 << "," << j+1 << ") : ";
+            cin >> arr[i][j];
+        }
+    }
+
+    int sum1 = 0;
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(i == j){
+                sum1 += arr[i][j];
+            }
+        }
+    }
+
+    cout << "Sum of Diagonal - 1 : " << sum1 << endl;
+
+    int sum2 = 0;
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(i + j == row - 1){
+                sum2 += arr[i][j];
+            }
+        }
+    }
+
+    cout << "Sum of Diagonal - 2 : " << sum2 << endl;
+
+    return 0;
+}
